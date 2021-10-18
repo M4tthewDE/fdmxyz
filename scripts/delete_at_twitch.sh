ce2a563e9a466459e1e091ae604f2d835452c0fd
M4tthewDE <mattider2@gmail.com>
2021-10-18 09:24:47 +0200

    remove tokens from scripts xD

--- scripts/delete_at_twitch.sh
+++ scripts/delete_at_twitch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X DELETE 'https://api.twitch.tv/helix/eventsub/subscriptions?id=135fdb95-b167-40a7-83fa-e9390def440f' -H 'Authorization: Bearer 8ehj169dnh3lkyj7q33x5e21lxyw49' -H 'Client-Id: e592cdqjjr51jcrqsir46htxpqaqv5'
+curl -X DELETE 'https://api.twitch.tv/helix/eventsub/subscriptions?id=ID' -H 'Authorization: Bearer TOKEN' -H 'Client-Id: CLIENT_ID'
--- scripts/get_from_twitch.sh
+++ scripts/get_from_twitch.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X GET 'https://api.twitch.tv/helix/eventsub/subscriptions' -H 'Authorization: Bearer 8ehj169dnh3lkyj7q33x5e21lxyw49' -H 'Client-Id: e592cdqjjr51jcrqsir46htxpqaqv5'
+curl -X GET 'https://api.twitch.tv/helix/eventsub/subscriptions' -H 'Authorization: Bearer TOKEN' -H 'Client-Id: CLIENT_ID'
